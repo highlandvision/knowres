97cbd0de99ba814183bfb3e02a7b787ffd6f24d8
Hazel Wilson <hazel@highlandvision.com>
2023-12-25 16:22:11 +0000

    v43-Destinations Page

--- modules/mod_knowres_destination/tmpl/default.php
+++ modules/mod_knowres_destination/tmpl/default.php
@@ -15,34 +15,35 @@ use HighlandVision\KR\SiteHelper;
 
 $Itemid = SiteHelper::getItemId('com_knowres', 'properties', ['region_id' => $params->get('region_id')]);
 $link   = KrMethods::route('index.php?option=com_knowres&view=properties&Itemid=' . $Itemid . '&region_id='
-	. $params->get('region_id'));
+                           . $params->get('region_id'));
 $text   = KrMethods::sprintf('MOD_KNOWRES_DESTINATION_VIEW_PROPERTIES', $destination);
 ?>
 
 <div class="kr-destinations">
 	<div class="grid-x grid-margin-x">
 		<div class="small-12 cell">
-			<?php $options = ['src'    => $params->get('image'),
-			                  'alt'    => $destination,
-			                  'class'  => 'th responsive'
+			<a href="<?php echo $link; ?>">
+				<?php $options = ['src'   => $params->get('image'),
+				                  'alt'   => $destination,
+				                  'class' => 'th responsive'
 				];
-			?>
-			<?php echo KrMethods::render('joomla.html.image', $options); ?>
-			<br><br>
+				?>
+				<?php echo KrMethods::render('joomla.html.image', $options); ?>
+				<h2 class="h4">
+					<?php echo KrMethods::sprintf('MOD_KNOWRES_DESTINATION_VIEW_PROPERTIES', $destination); ?>
+				</h2>
+			</a>
 
-			<h3><?php echo $params->get('heading1'); ?></h3>
+			<h4><?php echo $params->get('heading1'); ?></h4>
 			<?php echo $params->get('text1'); ?>
-			<br>
 
-			<h3><?php echo $params->get('heading2'); ?></h3>
+			<h4><?php echo $params->get('heading2'); ?></h4>
 			<?php echo $params->get('text2'); ?>
-			<br>
 
-			<h3><?php echo $params->get('heading3'); ?></h3>
+			<h4><?php echo $params->get('heading3'); ?></h4>
 			<?php echo $params->get('text3'); ?>
-			<br>
 
-			<h3><?php echo $params->get('heading4'); ?></h3>
+			<h4><?php echo $params->get('heading4'); ?></h4>
 			<?php echo $params->get('text4'); ?>
 		</div>
 	</div>
