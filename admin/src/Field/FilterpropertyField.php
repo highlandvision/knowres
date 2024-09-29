f4389fdbb4f7099c4f768694f91212ed1982d839
Hazel Wilson <hazel@highlandvision.com>
2024-09-29 11:32:04 +0100

    v51-Admin Filtering

--- admin/src/Field/FilterpropertyField.php
+++ admin/src/Field/FilterpropertyField.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 use Exception;
 use HighlandVision\KR\Joomla\Extend\ListField as KrListField;
 use HighlandVision\KR\Session as KrSession;
+use Joomla\CMS\Form\Form;
 use RuntimeException;
 
 use function array_merge;
@@ -41,11 +42,12 @@ class FilterpropertyField extends KrListField
 		$properties  = $userData->properties;
 		$state       = self::getState($this->form);
 		$table       = $this->getAttribute('table');
+		$departure   = self::getDeparture($this->form);
 
 		if ($table != 'own')
 		{
 			$options = self::filteringForeign('#__knowres_property', $table, 'property_id', 'id',
-				'property_name', $state);
+				'property_name', $state, $departure);
 		}
 		else
 		{
@@ -65,5 +67,23 @@ class FilterpropertyField extends KrListField
 		}
 
 		return array_merge(parent::getOptions(), $options);
+	}
+
+	/**
+	 * Get value for state
+	 *
+	 * @param  Form  $form  The form object
+	 *
+	 * @since  5.1.0
+	 * @return string
+	 */
+	public static function getDeparture(Form $form): string
+	{
+		$departure = '';
+		if (isset($form->getData()->get('filter', [])->departure)) {
+			$departure = $form->getData()->get('filter', [])->departure;
+		}
+
+		return $departure;
 	}
 }
