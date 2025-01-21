4a1f70885c02f03f50e732a72679f4de428c4ac8
Hazel Wilson <hazel@highlandvision.com>
2025-01-21 15:24:42 +0000

    v51-Dashboard Statement View

--- site/src/View/Dashboard/StatementView.php
+++ site/src/View/Dashboard/StatementView.php
@@ -12,10 +12,9 @@ namespace HighlandVision\Component\Knowres\Site\View\Dashboard;
 defined('_JEXEC') or die;
 
 use Exception;
-use HighlandVision\KR\Framework\KrFactory;
 use HighlandVision\KR\Joomla\Extend\HtmlView as KrHtmlView;
-use HighlandVision\KR\Logger;
-use HighlandVision\KR\SiteHelper;
+use JetBrains\PhpStorm\NoReturn;
+use function jexit;
 
 /**
  * Guest statement view
@@ -33,31 +32,11 @@ class StatementView extends KrHtmlView\Site
 	 * @since  1.0.0
 	 * @return void
 	 */
-	public function display($tpl = null): void
+	#[NoReturn] public function display($tpl = null): void
 	{
-		try
-		{
-			list($guest_id, $contract_id) = SiteHelper::validateDashboardSession();
-			$this->audience = 'guest';
-			$this->item     = KrFactory::getAdminModel('contract')->getItem($contract_id);
-			$this->guest    = KrFactory::getAdminModel('guest')->getItem($guest_id);
-			$this->notes    = KrFactory::getListModel('contractnotes')->getForContract($this->item->id, 1);
-			$this->payments = KrFactory::getListModel('contractpayments')->getForContract($this->item->id);
-			$this->fees     = KrFactory::getListModel('contractfees')->getForContract($this->item->id);
-			[$this->balance, $this->balance_all] = KrFactory::getAdminModel('contractpayment')::setBalances($this->item,
-				$this->payments,
-				$this->fees);
+		$this->setLayout('statement');
 
-			$this->setLayout('statement');
-			parent::display($tpl);
-		}
-		catch (Exception $e)
-		{
-			Logger::logMe($e->getMessage(), 'error');
-			$this->setLayout('error');
-			parent::display($tpl);
-		}
-
+		parent::display($tpl);
 		jexit();
 	}
 }
